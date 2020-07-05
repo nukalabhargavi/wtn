aa1fd56a90b12fa5df97f530aac5a357269e12fa
asus <asus@DESKTOP-COVBJEA>
2020-07-05 18:40:07 +0530

    Milesone 1

--- MileStone1/src/com/wipro/numberproblems/numpro2.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.numberproblems;
-
-public class numpro2 {
-public static void main(String[] args) {
-	int a=Integer.parseInt(args[0]);
-	int b=Integer.parseInt(args[1]);
-	System.out.println(a>b?a:b);
-}
-}
